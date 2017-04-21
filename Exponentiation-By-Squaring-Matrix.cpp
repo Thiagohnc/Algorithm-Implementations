2e5b5017a4371b5b64bd3544c2060b054f1de8b4
Thiagohnc <thiagohnc@hotmail.com>
2017-04-20 22:04:40 -0300

    Alg1

---
+++ Exponentiation-By-Squaring-Matrix.cpp
@@ -0,0 +1,72 @@
+/**	@Author: Thiago Henrique Neves Coelho
+  *	Date: 20/04/2017
+  * Algorithm: Matrix Exponentiation by Squaring
+  */
+
+#include <cstdio>
+#include <stdlib.h>
+ 
+typedef long long int ll;
+ 
+void clearMatrix(ll **matrix, int row) {
+	/* Free all allocated memory spaces in the matrix */
+	for(int i = 0; i < row; i++) free(matrix[i]);
+	free(matrix);
+}
+ 
+ll **multiplyMatrix(ll **matrix, ll **matrix2, int row, int col, int _row, int _col) {
+	/* Multiply matrix by matrix2 */
+	/* The sizes are: matrix(row x col) and matrix2(_row x _col) */
+	ll **aux;
+ 
+	aux = (ll **) malloc(sizeof(ll*)*row);
+	for(int i = 0; i < row; i++) aux[i] = (ll *) malloc(sizeof(ll)*_col);
+	/* aux(row x _col) will be the result of the multiplication */
+ 
+	if(col != _row) {
+		puts("Invalid matrix multiplication");
+		exit(1);
+	}
+ 
+	for(int i = 0; i < row; i++) {
+		for(int j = 0; j < _col; j++) {
+			aux[i][j] = 0;
+			for(int k = 0; k < col; k++) {
+				aux[i][j] += matrix[i][k] * matrix2[k][j];
+			}
+		}
+	}
+	return aux;
+}
+ 
+ll **exponentiate(ll **matrix, int n, int exp) {
+	ll **aux, **temp;
+ 
+	aux = (ll **) malloc(sizeof(ll*)*n);
+	for(int i = 0; i < n; i++) aux[i] = (ll *) malloc(sizeof(ll)*n);
+ 
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			aux[i][j] = matrix[i][j];
+			matrix[i][j] = (i == j ? 1 : 0);
+		}
+	}
+	/* matrix is now matrix^0 (Identity) */
+	/* aux stores the matrix to exponentiate itself in the loop */
+	
+	while(exp > 0) {
+		/* Square-And-Multiply Algorithm applied to matrices */
+		if(exp&1) {
+			temp = matrix;
+			matrix = multiplyMatrix(matrix, aux, n, n, n, n); // Multiply
+			clearMatrix(temp, n);
+		}
+		temp = aux;
+		aux = multiplyMatrix(aux, aux, n, n, n, n); // Square
+		clearMatrix(temp, n);
+		exp /= 2;
+	}
+	clearMatrix(aux, n);
+	
+	return matrix;
+}
